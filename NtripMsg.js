3b9ff05b664936abc4e94512c1a1b24216b4080a
IgorKilipenko <igorkili@mail.ru>
2019-02-04 05:38:01 +0700

    fix

---
+++ NtripMsg.js
@@ -0,0 +1,8 @@
+const constants = {
+    ICY200OK: "ICY 200 OK",
+    SOURCE200OK: "SOURCETABLE 200 OK",
+    ENDSOURCETABLE: "ENDSOURCETABLE",
+    BAD_PASS: "ERROR - Bad Password",
+    UNAUTHORIZED: "HTTP/1.0 401 Unauthorized",
+    CRLF: "\r\n"
+}
